7d26104d5385dc999c1507e35f892c62b178a749
utsgithub <uts_github@163.com>
2016-05-09 22:00:31 +1000

    Compile the assignment 1 ui design

---
+++ IMS_MVC/Controllers/EngController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using IMS_MVC.Models;
+
+namespace IMS_MVC.Controllers
+{
+
+    public class EngController : Controller
+    {
+        private ExtraDbContext db = new ExtraDbContext();
+        // GET: Eng
+        public ActionResult Index()
+        {
+            Response.Redirect("eng_dashboard");
+            return View();
+        }
+
+        public ActionResult eng_create_client()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult eng_create_client([Bind(Include = "Id,Name,Descriptive,DistrictId")] Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Clients.Add(client);
+                db.SaveChanges();
+                return RedirectToAction("eng_list_client");
+            }
+
+            return View(client);
+        }
+        public ActionResult eng_create_intervention()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult eng_create_intervention([Bind(Include = "Id,IntTypeId,ClientId,SetLabour,SetCost,UserId,IntDate,Status,Comments,Reamaining,VisitDate,ApprovedByUserId")] IntInfo intInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.IntInfos.Add(intInfo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(intInfo);
+        }
+        public ActionResult eng_dashboard()
+        {
+            return View();
+        }
+        public ActionResult eng_detail_client()
+        {
+            return View();
+        }
+        public ActionResult eng_detail_intervention()
+        {
+            return View();
+        }
+        public ActionResult eng_edit_intervention()
+        {
+            return View();
+        }
+        public ActionResult eng_list_client()
+        {
+            return View();
+        }
+        public ActionResult eng_list_intervention()
+        {
+            return View();
+        }
+
+        public ActionResult eng_list_int_via_client()
+        {
+            return View();
+        }
+    }
+}
---
+++ IMS_MVC/Controllers/ManController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IMS_MVC.Controllers
+{
+    public class ManController : Controller
+    {
+        // GET: Man
+        public ActionResult Index()
+        {
+            Response.Redirect("man_list_intervention");
+            return View();
+        }
+
+
+        public ActionResult man_edit_intervention() {
+            return View();
+        }
+
+        // Todo: propose;
+        // Todo: UserID;
+        public ActionResult man_list_intervention() {
+            return View();
+        }
+    }
+}
